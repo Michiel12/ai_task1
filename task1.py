74166b239d5aff8cae6feecaf61645442a863d98
Michiel Van Loy <91117399+Michiel12@users.noreply.github.com>
2023-10-03 22:29:06 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -135,7 +135,7 @@ if button and input3:
                     variable3_value += str(variable_value)
             
         # Print the solution
-        st.text("\u00A0\u00A0"+variable1_value)
+        st.text("&nbsp;&nbsp;"+variable1_value)
         st.text("+ "+variable2_value)
         st.text("-------")
         st.text("  "+variable3_value)
