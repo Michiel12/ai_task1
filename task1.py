028eccb94aa43132b565f8231086af5d2fa5c4ab
Michiel Van Loy <91117399+Michiel12@users.noreply.github.com>
2023-10-03 17:33:05 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -92,7 +92,8 @@ st.text(''.join(input3))
 problem = CspProblem(unique_variables, domains, constraints)
 
 output = backtrack(problem)
-st.text('\nSolutions:', output)
+solution = '\nSolutions:', output
+st.text(solution)
 
 # Define the input string
 input_string = ''.join(input1)+" "+''.join(input2)+" "+''.join(input3)
